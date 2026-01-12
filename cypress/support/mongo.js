ada257380ee809dd4aa4e06a972dc73d47e3c3de
Andreza Silva <andrezasilva744@gmail.com>
2026-01-11 22:26:07 -0300

    acesso mongo

--- cypress/support/mongo.js
+++ cypress/support/mongo.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb')
-const mongoUri = 'mongodb+srv://qa:pwd123@cluster0.xykpcik.mongodb.net/markdb?appName=Cluster0'
+const mongoUri = 'altera'
 const cliente = new MongoClient(mongoUri) //conectar no cluster
 
 async function connect(){
